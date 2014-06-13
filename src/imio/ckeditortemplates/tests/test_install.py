a5f7e503eca636c9688b1c6a9385e726f88face1
Benoît Suttor <benoit.suttor@imio.be>
2014-06-13 14:31:19 +0200

    add integration test

--- src/imio/ckeditortemplates/tests/test_install.py
+++ src/imio/ckeditortemplates/tests/test_install.py
@@ -7,6 +7,7 @@ from Products.CMFCore.utils import getToolByName
 
 from imio.ckeditortemplates.testing import IMIO_CKEDITORTEMPLATES_INTEGRATION
 
+
 class TestIntegration(unittest.TestCase):
     layer = IMIO_CKEDITORTEMPLATES_INTEGRATION
 
@@ -24,4 +25,13 @@ class TestIntegration(unittest.TestCase):
         pid = 'imio.ckeditortemplates'
         installed = [p['id'] for p in qi_tool.listInstalledProducts()]
         self.assertTrue(pid in installed,
-                    'package appears not to have been installed')
+                        'package appears not to have been installed')
+
+    def test_template_are_avaiable(self):
+        pass
+
+    def test_ckeditor_config(self):
+        ptools = getToolByName(self.portal, 'portal_properties')
+        cktools = ptools['ckeditor_properties']
+        self.assertTrue(cktools.filtering == "disabled")
+        self.assertTrue("{ name : ' Mail', element : 'p', attributes : { 'class' : 'mail' } },\n" in cktools.menuStyles)
