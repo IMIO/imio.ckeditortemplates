0f10d565afe9817bb7ac676f60143df5f02b8a44
Benoît Suttor <benoit.suttor@imio.be>
2014-08-11 10:28:16 +0200

    Back to development: 0.1.9

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = \
 
 setup(
     name='imio.ckeditortemplates',
-    version='0.1.8',
+    version='0.1.9.dev0',
     description="Initialisation of CKEditor templates for IMIO",
     long_description=long_description,
     # Get more from http://pypi.python.org/pypi?%3Aaction=list_classifiers
