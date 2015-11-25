fe4e3a8c14e80611ac49712c7b737e189a0db77e
Benoît Suttor <benoit.suttor@imio.be>
2015-11-25 16:46:04 +0100

    Add image for cktemplate for dx and at

--- src/imio/ckeditortemplates/setuphandlers.py
+++ src/imio/ckeditortemplates/setuphandlers.py
@@ -80,18 +80,18 @@ def setupTemplates(context):
 
 def add_images(folder_images):
     images = [
-        {'name': 'adresse.png', 'uuid': '62c875a43fca465aa7960312ec74cadr'},
-        {'name': 'courriel.png', 'uuid': '9b2970be20414acd81a7ee1d85732cou'},
-        {'name': 'fax.png', 'uuid': '922d4db790524e40865a72be3253fax'},
-        {'name': 'femme.png', 'uuid': 'f3961428681843f386f83a6ef173fem'},
-        {'name': 'gsm.png', 'uuid': '5716be3bb02246a097b752a92d4agsm'},
-        {'name': 'homme.png', 'uuid': 'a0adc656cdc94b18821fb84fc9dfhom'},
-        {'name': 'horloge.png', 'uuid': '13a4a96fc0254fbfbaf7d7b20cd5hor'},
-        {'name': 'lien.png', 'uuid': '6b334453028e4fb385bd9c4ee46clie'},
-        {'name': 'pdf-1.png', 'uuid': '1a3e3bfbab544f009faa7d527a0pdf'},
-        {'name': 'photo-exemple.jpg', 'uuid': 'f52fb2d7b47648b288a7ff897b5ppho'},
-        {'name': 'telephone.png', 'uuid': '3592b8b7a4f44b8c9d9c04595a0tel'},
-        {'name': 'localisation.png', 'uuid': '4b73aff9d5dc47df8e84998c8aec8357'},
+        {'name': u'adresse.png', 'uuid': '62c875a43fca465aa7960312ec74cadr'},
+        {'name': u'courriel.png', 'uuid': '9b2970be20414acd81a7ee1d85732cou'},
+        {'name': u'fax.png', 'uuid': '922d4db790524e40865a72be3253fax'},
+        {'name': u'femme.png', 'uuid': 'f3961428681843f386f83a6ef173fem'},
+        {'name': u'gsm.png', 'uuid': '5716be3bb02246a097b752a92d4agsm'},
+        {'name': u'homme.png', 'uuid': 'a0adc656cdc94b18821fb84fc9dfhom'},
+        {'name': u'horloge.png', 'uuid': '13a4a96fc0254fbfbaf7d7b20cd5hor'},
+        {'name': u'lien.png', 'uuid': '6b334453028e4fb385bd9c4ee46clie'},
+        {'name': u'pdf-1.png', 'uuid': '1a3e3bfbab544f009faa7d527a0pdf'},
+        {'name': u'photo-exemple.jpg', 'uuid': 'f52fb2d7b47648b288a7ff897b5ppho'},
+        {'name': u'telephone.png', 'uuid': '3592b8b7a4f44b8c9d9c04595a0tel'},
+        {'name': u'localisation.png', 'uuid': '4b73aff9d5dc47df8e84998c8aec8357'},
     ]
     import os
     from imio.ckeditortemplates import interfaces
@@ -100,15 +100,31 @@ def add_images(folder_images):
         if not folder_images.get(image):
             img_path = os.sep.join([package_path, "browser", "static", image['name']])
             img_file = open(img_path, 'r')
-            img = api.content.create(type="Image",
-                                     title=image['name'],
-                                     image=img_file,
-                                     container=folder_images,
-                                     language='fr')
-            img._setUID(image['uuid'])
+            if folder_images.portal_type == 'Image' and 'Dexterity' not in folder_images.meta_type:
+                img = api.content.create(type='Image',
+                                         title=image['name'],
+                                         image=img_file,
+                                         container=folder_images,
+                                         language='fr')
+            else:
+                # with deterity image
+                from plone.namedfile.file import NamedBlobImage
+                namedblobimage = NamedBlobImage(
+                    data=open(img_path, 'r').read(),
+                    filename=image['name']
+                )
+                img = api.content.create(type='Image',
+                                         title=image['name'],
+                                         image=namedblobimage,
+                                         container=folder_images,
+                                         language='fr')
+                # import ipdb; ipdb.set_trace()
+            if hasattr(img, '_setUID'):
+                img._setUID(image['uuid'])
             if api.content.get_uuid(obj=img) != image['uuid']:
                 # its a dexterity content
                 setattr(img, ATTRIBUTE_NAME, image['uuid'])
+
             img.reindexObject()
 
 
